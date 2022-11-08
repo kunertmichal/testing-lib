e270b05942bcf0e2fec656667c1cdbdf01ab5993
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2022-11-08 15:26:04 +0100

    encapsulate and isolate tests

--- simple.js
+++ simple.js
@@ -1,19 +1,31 @@
 const sum = (a, b) => a - b
 const subtract = (a, b) => a - b
 
-const result = sum(3, 7)
-const expected = 10
-expect(result).toBe(expected)
+test('sum adds numbers', () => {
+  const result = sum(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
 
-const result2 = subtract(7, 3)
-const expected2 = 4
-expect(result2).toBe(expected2)
+test('subtract subtracts numbers', () => {
+  const result = subtract(5, 2)
+  const expected = 3
+  expect(result).toBe(expected)
+})
 
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`✅ ${title}`)
+  } catch (error) {
+    console.error(`❌ ${error}`)
+  }
+}
 
 function expect(actual) {
   return {
     toBe(expected) {
-      if (result !== expected) {
+      if (actual !== expected) {
         throw new Error(`${actual} is not equal to ${expected}`)
       }
     },
